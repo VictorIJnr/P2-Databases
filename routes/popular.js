a30f49da90c85a5f22bac21be8c8026250366e0a
VictorIJnr <vi4@st-andrews.ac.uk>
2017-11-30 19:59:33 +0000

    Made some small changes

--- routes/popular.js
+++ routes/popular.js
@@ -7,9 +7,11 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/', function(req, res, next) {
-    console.log(req.body);
-    if (req.body.reviews) mysql.bookReviews(res, req.body.review);
-    if (req.body.purchase) mysql.buyBook(res, req.body.review);
+    if (req.body.reviews) mysql.bookReviews(res, req.body.isbn);
+    if (req.body.purchase) {
+        var isbn = encodeURIComponent(req.body.isbn);
+        res.redirect("/CS3101/purchases?isbn=" + isbn);
+    }
 });
 
 module.exports = router;
--- routes/purchases.js
+++ routes/purchases.js
@@ -2,6 +2,10 @@ var express = require('express');
 var router = express.Router();
 var mysql = require('../mysql');
 
-
+router.get("/", function(req, res, next) {
+    console.log(req.query.isbn);
+    // mysql.buyBook(res, req.body.isbn);
+    res.render('purchases');
+});
 
 module.exports = router;
